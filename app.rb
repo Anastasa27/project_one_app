1cb08c25643015859fc721b131456cefac49f00d
anastasa27 <anastasiakonecky@gmail.com>
2014-09-07 16:07:23 -0400

    corrected API text layout issues

--- app.rb
+++ app.rb
@@ -58,18 +58,8 @@ end
   #######################
   #Routes
   #######################
-  before('/profile/new') do
-    params = {
-      :twitter => "true",
-      :idream_books => "true",
-      :book_browse_news => "true",
-      :ny_times => "true",
-    }
-    TWITTER = params[:twitter]
-    IDREAMBOOKS = params[:idream_books]
-    BOOK_BROWSE_NEWS = params[:book_browse_news]
-    NY_TIMES = params[:ny_times]
-  end
+
+
 
   get('/') do
     render(:erb, :index)
@@ -89,7 +79,7 @@ end
       #new york times bestsellers
       @ny_times_response = HTTParty.get("http://api.nytimes.com/svc/books/v2/lists.json?list=hardcover-fiction&api-key=a334d90853f03ea079bda17f9f0fc548:17:69767462").to_json
       @parsed_version = JSON.parse(@ny_times_response)
-      twitter
+      #twitter
       @tweets = []
       TWITTER_CLIENT.search("twitterbooks", :result_type => "recent").take(20).each_with_index do |tweet|
        @tweets.push(tweet.text)
@@ -102,38 +92,24 @@ end
       open(url) do |rss|
       @feed = RSS::Parser.parse(rss)
       end
-    render(:erb, :index)
+    render(:erb, :dashboard)
   end
 
+  # def add_user_profile_info(new_user)
+  #   new_user = $redis.keys("*new_user*")
+  #   key = new_user + 1
+  #   $redis.set(key, new_user.to_json)
+  #   redirect to("/profile/new")
+  # end
 
   post('/profile/new') do
-    # if params[:twitter] == nil
-    #   TWITTER = false
-    # else
-    #   TWITTER = "true"
-    # end
-    # if params[:idream_books] == nil
-    #   IDREAMBOOKS = false
-    # else
-    #   IDREAMBOOKS = "true"
-    # end
-    # if params[:book_browse_news] == nil
-    #   BOOK_BROWSE_NEWS = false
-    # else
-    #   BOOK_BROWSE_NEWS = "true"
-    # end
-    # if params[:ny_times] == nil
-    #   NY_TIMES = false
-    # else
-    #   NY_TIMES = "true"
-    # end
-    # new_profile = {
-    #   :user_name  => params[:user_name],
-    #   :email => params[:user_email]
-    # }
-    # @@user_profile.push(new_profile)
-    #  logger.info @@profile
-    # redirect to('/dashboard')
+
+    new_user = {
+      :"user_name" =>  params["name"],
+      :"email"     =>  params["email"],
+    }
+    add_user_profile_info(new_user)
+    redirect to('/dashboard')
   end
 
   # delete('/profile/:id') do
