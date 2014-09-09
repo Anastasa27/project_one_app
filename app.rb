0fdb6396fbd9a93b50e452b73383b55b60c0b87a
anastasa27 <anastasiakonecky@gmail.com>
2014-09-09 18:27:25 -0400

    modified fonts in css file

--- app.rb
+++ app.rb
@@ -24,7 +24,7 @@ class App < Sinatra::Base
     $redis = Redis.new({:host => uri.host,
                         :port => uri.port,
                        :password => uri.password})
-    @@user_profiles = []
+    $redis.setnx("user_counter", 1)
   end
 
   before do
@@ -39,13 +39,13 @@ class App < Sinatra::Base
  #########################
  #API KEYS
  #########################
-WUNDERGROUND_API_KEY = ENV['8df98bbf67d1296c']
-TWITTER_API_KEY = 'KAHgTJJDsE8EmiQTMvTWc6mUQ'
-TWITTER_SECRET_KEY = 'TMbxpVO5NisM0E0QN6ese8i2QieFsaMEUgZlTJPuT8klVXPVOF'
-TWITTER_ACCESS_TOKEN = '2790859370-RiHbePsaHNIjdmSV4vWfUMUvCro1mJA4xhbDxyy'
-TWITTER_ACCESS_SECRET_TOKEN = 'vMNh7eTX5Qy99DWXDMs5Vc2HpXPcX64EkpqLex8RdX5Xe'
-NY_TIMES_API_KEY = 'a334d90853f03ea079bda17f9f0fc548:17:69767462'
-IDREAMBOOKS_API_KEY = '31b59ece20986033f5307f7907f7d94e87b4a45f'
+  WUNDERGROUND_API_KEY = ENV['8df98bbf67d1296c']
+  TWITTER_API_KEY = 'KAHgTJJDsE8EmiQTMvTWc6mUQ'
+  TWITTER_SECRET_KEY = 'TMbxpVO5NisM0E0QN6ese8i2QieFsaMEUgZlTJPuT8klVXPVOF'
+  TWITTER_ACCESS_TOKEN = '2790859370-RiHbePsaHNIjdmSV4vWfUMUvCro1mJA4xhbDxyy'
+  TWITTER_ACCESS_SECRET_TOKEN = 'vMNh7eTX5Qy99DWXDMs5Vc2HpXPcX64EkpqLex8RdX5Xe'
+  NY_TIMES_API_KEY = 'a334d90853f03ea079bda17f9f0fc548:17:69767462'
+  IDREAMBOOKS_API_KEY = '31b59ece20986033f5307f7907f7d94e87b4a45f'
   #######################
   #GIT HUB OAUTH API KEYS
   #######################
@@ -57,12 +57,12 @@ IDREAMBOOKS_API_KEY = '31b59ece20986033f5307f7907f7d94e87b4a45f'
   ########################
   # Twitter
   ########################
-TWITTER_CLIENT = Twitter::REST::Client.new do |config|
-  config.consumer_key        = TWITTER_API_KEY
-  config.consumer_secret     = TWITTER_SECRET_KEY
-  config.access_token        = TWITTER_ACCESS_TOKEN
-  config.access_token_secret = TWITTER_ACCESS_SECRET_TOKEN
-end
+  TWITTER_CLIENT = Twitter::REST::Client.new do |config|
+    config.consumer_key        = TWITTER_API_KEY
+    config.consumer_secret     = TWITTER_SECRET_KEY
+    config.access_token        = TWITTER_ACCESS_TOKEN
+    config.access_token_secret = TWITTER_ACCESS_SECRET_TOKEN
+  end
   #######################
   #Routes
   #######################
@@ -103,12 +103,11 @@ end
 
 
   get('/profile/new') do
-     @user1 = JSON.parse($redis["user1"])
     render(:erb, :profile_info_form, :template => :layout)
   end
 
   get('/profile') do
-    render(:erb, :profile, :template => :layout)
+    render(:erb, :profile_info_form, :template => :layout)
   end
 
   get('/twitter_books') do
@@ -134,23 +133,22 @@ end
       @weather = HTTParty.get("http://api.wunderground.com/api/8df98bbf67d1296c/conditions/q/#{@state}/#{@city}.json")
       @temp_in_farh = @weather["current_observation"]["temp_f"]
       #new york times bestsellers
-      @user1 = JSON.parse($redis["user1"])
+      @user1 = get_user(session[:user_id_num])
       @ny_times_response = HTTParty.get("http://api.nytimes.com/svc/books/v2/lists.json?list=hardcover-fiction&api-key=a334d90853f03ea079bda17f9f0fc548:17:69767462").to_json
       @parsed_version = JSON.parse(@ny_times_response)
       #twitter
-      @tweets = []
+       @tweets = []
       TWITTER_CLIENT.search("twitterbooks", :result_type => "recent").take(20).each_with_index do |tweet|
        @tweets.push(tweet.text)
       end
-      #idreambooks
-      @book_reviews = HTTParty.get("http://idreambooks.com/api/publications/recent_recos.json?key=31b59ece20986033f5307f7907f7d94e87b4a45f&slug=fiction").to_json
-      @parsed_reviews = JSON.parse(@book_reviews)
-      #bookbrowse news rss
-      url = 'https://www.bookbrowse.com/rss/book_news.rss'
-      open(url) do |rss|
-      @feed = RSS::Parser.parse(rss)
-      end
-      # binding.pry
+        #idreambooks
+        @book_reviews = HTTParty.get("http://idreambooks.com/api/publications/recent_recos.json?key=31b59ece20986033f5307f7907f7d94e87b4a45f&slug=fiction").to_json
+        @parsed_reviews = JSON.parse(@book_reviews)
+        #bookbrowse news rss
+          url = 'https://www.bookbrowse.com/rss/book_news.rss'
+          open(url) do |rss|
+          @feed = RSS::Parser.parse(rss)
+        end
     render(:erb, :dashboard)
   end # ends get /dashboard
 
@@ -159,7 +157,9 @@ end
   end
 
   post('/feeds') do
+    session[:user_id_num] = nil
     user_hash = {
+      :user_id => $redis.get("user_counter"),
       :user_name => params[:user_name],
       :email => params[:email],
       :image_url => params[:image_url],
@@ -169,25 +169,24 @@ end
       :book_browse_news => params[:book_browse_news],
     }
 
-    @@user_profiles.push(user_hash)
-    @@user_profiles.each_with_index do |user_profile, index|
-    $redis.set("user1", user_hash.to_json)
-    $redis.keys.each do |key, value|
+    session[:user_id_num] = $redis.get("user_counter")
+      $redis.set("user:#{$redis.get("user_counter")}", user_hash.to_json)
+       $redis.incr("user_counter")
+     redirect('/dashboard')
     end
-    # binding.pry
-    redirect('/dashboard')
-  end
 
-get('/profile/edit')
-  redirect to('/profile/new')
-end
+  post('/profile/edit') do
+    redirect to('/profile/new')
+  end
 
 
   get('/profile/:id') do
-    @user_profiles = @@user_profiles
-    @index = params[:id].to_i - 1
-
+    @user_profiles = get_user(params[:id])
     render(:erb, :profile, :template => :layout)
+  end
+
+  def get_user(user_id)
+    JSON.parse($redis.get("user:#{user_id}"))
   end
 
 
