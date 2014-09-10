850b845e97609d9272598466073c6312fcb18f8e
anastasa27 <anastasiakonecky@gmail.com>
2014-09-10 09:38:02 -0400

    modified user1 redis.set in seeds file

--- seeds.rb
+++ seeds.rb
@@ -7,25 +7,22 @@ $redis = Redis.new({:host => uri.host,
                     :port => uri.port,
                     :password => uri.password})
 
-$redis.flushdb
 
 # file_contents = File.read('user_data.json')
 # ruby_object = JSON.parse(file_contents)
 
 
-    user1 = [
-      {
+    user1 = {
       "user_name" => "Anastasia Konecky",
       "email" => "anastasiakonecky@gmail.com",
       "ny_times" => "on",
       "twitter_books" => "on",
       "idream_books" => "on",
-      "book_browse_news" => "on" }
-      ]
+      "book_browse_news" => "on"
+    }
+
+  $redis.set("user:1", user1.to_json)
 
 
 
-ruby_object[user1].each_with_index do |user1, index|
-  $redis.set("user1:#{index}", user1.to_json)
-end
 
