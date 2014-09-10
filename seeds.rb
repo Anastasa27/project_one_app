7142621fa525a8ddccd44bbbd440f473c77bc681
anastasa27 <anastasiakonecky@gmail.com>
2014-09-10 09:26:42 -0400

    modified seeds.rb

--- seeds.rb
+++ seeds.rb
@@ -10,11 +10,23 @@ $redis = Redis.new({:host => uri.host,
 $redis = Redis.new
 $redis.flushdb
 
-file_contents = File.read('user_data.json')
-ruby_object = JSON.parse(file_contents)
+# file_contents = File.read('user_data.json')
+# ruby_object = JSON.parse(file_contents)
 
 
-ruby_object["user1"].each_with_index do |"user1", index|
+      "user1" = [
+      {
+      "user_name" => "Anastasia Konecky",
+      "email" => "anastasiakonecky@gmail.com",
+      "ny_times" => "on",
+      "twitter_books" => "on",
+      "idream_books" => "on",
+      "book_browse_news" => "on" }
+      ]
+
+
+
+ruby_object["user1"].each_with_index do |user1, index|
   $redis.set("user1:#{index}", user1.to_json)
 end
 
